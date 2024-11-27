46acf349fe89dfbddf10a3c38a24fd9b0fe578f8
hyeon <hyeon970315@gmail.com>
2024-11-27 11:05:34 +0900

    React - Hook 이용한 ui demo (1)

---
+++ 09_ui_demo/src/pages/create_element_ex/index.js
@@ -0,0 +1,16 @@
+import React, {memo} from "react";
+import styled from 'styled-components';
+
+const CreateElementExContainer = styled.div`
+
+`;
+
+const CreateElementEx = memo( () => {
+    return(
+        <CreateElementExContainer>
+            <h2> CreateElementEx </h2>
+        </CreateElementExContainer>
+    );
+} );
+
+export default CreateElementEx;
